51a99cec66324ed9efbadf1b530888f46d904748
UlaşDK <udk97@hotmail.com>
2022-09-04 22:47:36 +0300

    Initialize project using Create React App

---
+++ src/components/card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <button className="btn btn secondary"></button>
+    </div>
+  )
+}
+
+export default Card
