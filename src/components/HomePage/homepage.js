4b96ef4190e9c2954e9ba3cb40c55326f0447b16
UlaşDK <udk97@hotmail.com>
2022-09-06 01:43:38 +0300

    Input Page added

--- src/components/HomePage/homepage.js
+++ src/components/HomePage/homepage.js
@@ -1,12 +1,12 @@
-import PaymentButtons from "../Buttons/PaymentOptions"
 import Navbar from "../Navbar/Navbar";
+import InputPage from "../InputPage/Inputpage";
 import Footer from "../Footer/Footer";
 
 function HomePage() {
   return (
     <div>
       <Navbar/>
-      <PaymentButtons/>
+      <InputPage/>
       <Footer/>
     </div>
   );
--- src/components/InputPage/inputpage.js
+++ src/components/InputPage/inputpage.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import "./Inputpage.css"
 
 function InputPage() {
   return (
-    <div>
-    </div>
-  )
+        <div className="container">
+          <div className="card">
+            <input placeholder="Credit Amount" type="number"></input>
+            <input placeholder="Installment Amount" type="number"></input>
+            <input placeholder="Profit Rate" type="number"></input>
+            <input placeholder="Tax (BSMV)" type="number"></input>
+            <input placeholder="Tax (KKDf)" type="number"></input>
+            <p className="homepage-text">PLEASE CHOOSE YOUR PAYMENT METHOD</p>
+            <button className="btn btn-info">Weekly</button>
+            <button className="btn btn-warning">Monthly</button>
+            <button className="btn btn-danger">Annual</button>
+          </div>
+        </div>
+  );
 }
 
 export default InputPage
