714379353bd5aa182ad249c1d42b585eb53e5f35
UlaşDK <udk97@hotmail.com>
2022-09-06 02:35:53 +0300

    Input page updates

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import "./Navbar.css"
 function Navbar() {
   return (
     <div>
-      <nav className="navbar bg-primary">
+      <nav className="navbar bg-dark">
         <a className="navbar-brand" href="#" style={{ color: "inherit" }}>
           <img
             src="https://media-exp1.licdn.com/dms/image/C4D0BAQFJODlNe80N1Q/company-logo_200_200/0/1648554411362?e=1670457600&v=beta&t=_4s8PtWmnC4i2Xta5BvrZuk-FCbis4AkejHr930tWVg"
