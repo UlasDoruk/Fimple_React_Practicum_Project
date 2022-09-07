1e6b8f41f04cd2609521affad7c2ccf2128eabcb
UlaşDK <udk97@hotmail.com>
2022-09-07 18:25:44 +0300

    Formulacontext added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import HomePage  from './components/Pages/homepage';
 import Table from "./components/Pages/Table";
 import {Routes,Route} from "react-router-dom"
 import {InputProvider} from "./components/Context/InputContext"
+import {FormulaProvider} from "./components/Context/FormulaContext"
 
 function App() {
   return (
     <div className="App">
-      <InputProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/Table" element={<Table />}></Route>
-        </Routes>
-      </InputProvider>
+      <FormulaProvider>
+        <InputProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/Table" element={<Table />}></Route>
+          </Routes>
+        </InputProvider>
+      </FormulaProvider>
     </div>
   );
 }
