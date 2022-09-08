1b2789d909571c0b797baa81553319bef718341e
UlaşDK <udk97@hotmail.com>
2022-09-08 15:38:04 +0300

    Looping table

--- src/components/Pages/Table.js
+++ src/components/Pages/Table.js
@@ -1,6 +1,6 @@
 import "./Table.css"
 import ResetButton from "../Buttons/ResetButton";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import InputContext from "../Context/InputContext"
 
 function Table() {
@@ -10,6 +10,12 @@ function Table() {
   const { installment } = useContext(InputContext);
   const { kkdf } = useContext(InputContext);
   const { bsmv } = useContext(InputContext);
+
+  let  newinstallment = parseInt(installment)
+  const newarr = []
+  Array(newinstallment).fill(0).forEach((x,i)=>{
+    newarr.push(i+1)
+  })
 
   const newprofit = parseFloat(credit * profit).toFixed(2);
   const newkkdf = parseFloat(credit * kkdf).toFixed(2);
@@ -23,6 +29,25 @@ function Table() {
   const main = parseFloat(price - newprofit - newbsmv - newkkdf).toFixed(2);
   const remain = (credit-main).toFixed(2)
 
+  const renderTable = ()=>{
+   return(
+    <React.Fragment>
+      {newarr.map((num,index)=>{
+        return (
+          <tr>
+            <th scope="row">{num}</th>
+            <td>{price}</td>
+            <td>{main}</td>
+            <td>{remain}</td>
+            <td>{newprofit}</td>
+            <td>{newkkdf}</td>
+            <td>{newbsmv}</td>
+          </tr>
+        );
+      })}
+    </React.Fragment>
+   )
+  }
 
   return (
     <div className="container">
@@ -39,15 +64,7 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>{price}</td>
-            <td>{main }</td>
-            <td>{remain}</td>
-            <td>{newprofit}</td>
-            <td>{newkkdf}</td>
-            <td>{newbsmv}</td>
-          </tr>
+          {renderTable()}
         </tbody>
       </table>
       <ResetButton />
