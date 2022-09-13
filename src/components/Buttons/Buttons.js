8cf7b8de2d4dcd208526a02f199b17024bbc9177
UlaşDK <udk97@hotmail.com>
2022-09-13 16:52:59 +0300

    Css updates

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -4,9 +4,9 @@ function Buttons() {
   return (
     <div className="buttons">
       <p className="homepage-text">PLEASE CHOOSE YOUR PAYMENT METHOD</p>
-      <button className="btn btn-danger">Weekly</button>
-      <button className="btn btn-danger">Monthly</button>
-      <button className="btn btn-danger">Annual</button>
+      <button className="btn btn-danger">WEEKLY</button>
+      <button className="btn btn-danger">MONTHLY</button>
+      <button className="btn btn-danger">ANNUAL</button>
     </div>
   );
 }
